a0f41a61302ab6107e6910160c968ef5cb1e1bea
marleyschneiderr <marley33@uw.edu>
2023-04-23 21:47:41 -0700

    changing syntax, adding arrow function, using let/const not var, callback function

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -3,37 +3,37 @@ const fs = require("fs");
 var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
 module.exports = function (app) {
-  app.get("/api/notes", function (req, res) {
+  app.get("/api/notes", (req, res) => {
     res.json(data);
   });
 
-  app.get("/api/notes/:id", function (req, res) {
+  app.get("/api/notes/:id", (req, res) => {
     res.json(data[Number(req.params.id)]);
   });
 
-  app.post("/api/notes", function (req, res) {
-    let addNote = req.body;
-    let differentId = data.length.toString();
+  app.post("/api/notes", (req, res) => {
+    const addNote = req.body;
+    const differentId = data.length.toString();
     console.log(differentId);
     addNote.id = differentId;
     data.push(addNote);
 
-    fs.writeFileSync("./db/db.json", JSON.stringify(data), function (err) {
+    fs.writeFileSync("./db/db.json", JSON.stringify(data), (err) => {
       if (err) throw err;
     });
 
     res.json(data);
   });
 
-  app.delete("/api/notes/:id", function (req, res) {
-    let entryId = req.params.id;
+  app.delete("/api/notes/:id", (req, res) => {
+    const entryId = req.params.id;
     let addId = 0;
     console.log(`Deleting note with id ${entryId}`);
     data = data.filter((nowNote) => {
       return nowNote.id != entryId;
     });
 
-    for (nowNote of data) {
+    for (const nowNote of data) {
       nowNote.id = addId.toString();
       addId++;
     }
