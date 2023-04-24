34dc063eab77d768125dad0303cc3a822ff5210c
marleyschneiderr <marley33@uw.edu>
2023-04-23 17:54:46 -0700

    Creating correct links in sever.js file, getting server to listen

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const PORT = process.env.PORT || 3001;
+// dependancies
+const express = require("express");
+// loads the fs moduel in Node.js, a built in Node.js module that provides an API working w/ the file system
+const fs = require("fs");
 
-const apiRoutes = require('/')
+// Express app set up
+var app = express();
+// || is a fallback value in case process.env.PORT is not defined/is undefined, the fallback is 3001 in this case
+var PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/public/assets", express.static(_dirname + "/public/assets"));
+
+require("./routes/htmlRoutes/htmlRoutes.js")(app);
+require("./routes/apiRoutes/apiRoutes.js")(app);
+
+// starting the server to listen and display
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
