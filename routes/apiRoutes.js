9835072163b169915a21926ca1af0a97c4b80614
marleyschneiderr <marley33@uw.edu>
2023-05-29 20:32:12 -0700

    Removing API in routes and deleting second export

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,16 +3,15 @@ const router = require('express').Router();
 // reads JSON file, reads content, returns a buffer
 var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
-module.exports = function (router) {
-  router.get("/api/notes", (req, res) => {
+  router.get("/notes", (req, res) => {
     res.json(data);
   });
 
-  router.get("/api/notes/:id", (req, res) => {
+  router.get("/notes/:id", (req, res) => {
     res.json(data[Number(req.params.id)]);
   });
 
-  router.post("/api/notes", (req, res) => {
+  router.post("/notes", (req, res) => {
     const addNote = req.body;
     const differentId = data.length.toString();
     console.log(differentId);
@@ -26,7 +25,7 @@ module.exports = function (router) {
     res.json(data);
   });
 
-  router.delete("/api/notes/:id", (req, res) => {
+  router.delete("/notes/:id", (req, res) => {
     const entryId = req.params.id;
     let addId = 0;
     console.log(`Deleting note with id ${entryId}`);
@@ -42,6 +41,5 @@ module.exports = function (router) {
     fs.writeFileSync("./db/db.json", JSON.stringify(data));
     res.json(data);
   });
-};
 
 module.exports = router;
